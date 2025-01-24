77338f480842d2db639d2cc496396e0f1df61ff1
Emilio-prog <145426876+Emilio-prog@users.noreply.github.com>
2025-01-24 16:47:43 +0100

    commit rama 1

--- proyecto_git/src/proyecto1/principal.java
+++ proyecto_git/src/proyecto1/principal.java
@@ -5,6 +5,7 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("prueba");
+		System.out.print("Cambios hechos en la rama 1");
 	}
 
 }
