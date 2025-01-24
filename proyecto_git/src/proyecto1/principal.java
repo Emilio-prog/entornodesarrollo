4d88762cc33b5ffec332a3de257f78346757ff67
Emilio-prog <145426876+Emilio-prog@users.noreply.github.com>
2025-01-24 16:03:35 +0100

    segundo comic

--- proyecto_git/src/proyecto1/principal.java
+++ proyecto_git/src/proyecto1/principal.java
@@ -3,8 +3,8 @@ package proyecto1;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola mundo");
+		System.out.println("prueba");
 	}
 
 }
